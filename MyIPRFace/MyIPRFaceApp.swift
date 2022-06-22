437b74112af2eaceb25399cd367209b710a4f20e
Katios <kate.chonkina@gmail.com>
2022-06-22 10:52:15 +0600

    if device is not supported

--- MyIPRFace/MyIPRFaceApp.swift
+++ MyIPRFace/MyIPRFaceApp.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import iOSDevPackage
-
+import ARKit
 
 @main
 struct MyIPRFaceApp: App {
@@ -8,8 +8,12 @@ struct MyIPRFaceApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationControllerView(transition: .custom(.slide, .slide)) {
+                if ARFaceTrackingConfiguration.isSupported {
                 SelectionOfSmiles()
-            }
+                } else {
+                    Text("Face tracking is not supported on this device")
+                }
         }
     }
+}
 }
